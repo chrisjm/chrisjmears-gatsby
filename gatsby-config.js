147b48cd55eb5eff183a6f24850aa9a27ec0e422
Chris Mears <chris.mears@gmail.com>
2019-07-10 15:26:19 -0700

    Compile blog posts

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,9 +2,29 @@ module.exports = {
   siteMetadata: {
     title: `Gatsby Starter Tailwind`,
     description: `Gatsby starter styled with Tailwind`,
-    author: `@taylorbryant`
+    author: `@taylorbryant`,
   },
   plugins: [
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        path: `${__dirname}/content/blog`,
+        name: `blog`,
+      },
+    },
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        path: `${__dirname}/content/assets`,
+        name: `assets`,
+      },
+    },
+    {
+      resolve: `gatsby-transformer-remark`,
+      options: {
+        plugins: [],
+      },
+    },
     `gatsby-plugin-react-helmet`,
     {
       resolve: `gatsby-plugin-manifest`,
@@ -15,16 +35,16 @@ module.exports = {
         background_color: `#ffffff`,
         theme_color: `#4dc0b5`,
         display: `minimal-ui`,
-        icon: `src/images/tailwind-icon.png`
-      }
+        icon: `src/images/tailwind-icon.png`,
+      },
     },
     `gatsby-plugin-postcss`,
     {
-      resolve: "gatsby-plugin-purgecss",
+      resolve: 'gatsby-plugin-purgecss',
       options: {
         tailwind: true,
-        purgeOnly: ["src/css/style.css"]
-      }
-    }
-  ]
+        purgeOnly: ['src/css/style.css'],
+      },
+    },
+  ],
 };
