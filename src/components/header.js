c7be896c265650a5ba56b5cd63de822ffd244b95
Chris Mears <chris.mears@gmail.com>
2019-07-29 15:45:33 -0700

    Remove Roadmapping link

--- src/components/header.js
+++ src/components/header.js
@@ -6,72 +6,59 @@ function Header({ bgColor = 'bg-blue-500', linkColor = 'text-white' }) {
   const [isExpanded, toggleExpansion] = useState(false);
 
   return (
-    <div>
-      <header className={bgColor}>
-        <div className="container mx-auto p-5 md:p-10">
-          <div className="content">
-            <div className="flex flex-col md:flex-row justify-center content-center">
-              <div className="branding flex-1">
-                <div className="text-3xl md:text-4xl font-black mb-1 md:mb-0">
-                  <Link to="/" className={`${linkColor} no-underline antialiased`}>
-                    Chris J Mears
-                  </Link>
-                </div>
+    <header className={bgColor}>
+      <div className="container mx-auto p-5 md:p-10">
+        <div className="content">
+          <div className="flex flex-col md:flex-row justify-center content-center">
+            <div className="branding flex-1">
+              <div className="text-3xl md:text-4xl font-black mb-1 md:mb-0">
+                <Link to="/" className={`${linkColor} no-underline antialiased`}>
+                  Chris J Mears
+                </Link>
               </div>
-              <div className="menu flex-1 md:text-right">
-                <nav>
-                  <div className="flex flex-wrap items-center justify-end">
-                    <button
-                      className="block md:hidden border border-white flex items-center px-3 py-2 rounded text-white"
-                      onClick={() => toggleExpansion(!isExpanded)}
+            </div>
+            <div className="menu flex-1 md:text-right">
+              <nav>
+                <div className="flex flex-wrap items-center justify-end">
+                  <button
+                    className="block md:hidden border border-white flex items-center px-3 py-2 rounded text-white"
+                    onClick={() => toggleExpansion(!isExpanded)}
+                  >
+                    <svg
+                      className="fill-current h-3 w-3"
+                      viewBox="0 0 20 20"
+                      xmlns="http://www.w3.org/2000/svg"
                     >
-                      <svg
-                        className="fill-current h-3 w-3"
-                        viewBox="0 0 20 20"
-                        xmlns="http://www.w3.org/2000/svg"
-                      >
-                        <title>Menu</title>
-                        <path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z" />
-                      </svg>
-                    </button>
+                      <title>Menu</title>
+                      <path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z" />
+                    </svg>
+                  </button>
 
-                    <div
-                      className={`${
-                        isExpanded ? `block` : `hidden`
-                      } md:block md:flex md:items-center w-full md:w-auto`}
-                    >
-                      <ul className="list-reset mt-5 inline-block">
-                        <li className="inline-block mr-5 mb-5">
-                          <Link className={`${linkColor} no-underline antialiased`} to="/blog">
-                            Blog
-                          </Link>
-                        </li>
-                        <li className="inline-block mr-5 mb-5">
-                          <Link
-                            className={`${linkColor} no-underline antialiased`}
-                            to="/#newsletter"
-                          >
-                            Newsletter
-                          </Link>
-                        </li>
-                        <li className="inline-block mr-5 mb-5">
-                          <Link
-                            className={`${linkColor} no-underline antialiased`}
-                            to="/roadmapping"
-                          >
-                            Roadmapping
-                          </Link>
-                        </li>
-                      </ul>
-                    </div>
+                  <div
+                    className={`${
+                      isExpanded ? `block` : `hidden`
+                    } md:block md:flex md:items-center w-full md:w-auto`}
+                  >
+                    <ul className="list-reset mt-5 inline-block">
+                      <li className="inline-block mr-5 mb-5">
+                        <Link className={`${linkColor} no-underline antialiased`} to="/blog">
+                          Blog
+                        </Link>
+                      </li>
+                      <li className="inline-block mr-5 mb-5">
+                        <Link className={`${linkColor} no-underline antialiased`} to="/#newsletter">
+                          Newsletter
+                        </Link>
+                      </li>
+                    </ul>
                   </div>
-                </nav>
-              </div>
+                </div>
+              </nav>
             </div>
           </div>
         </div>
-      </header>
-    </div>
+      </div>
+    </header>
   );
 }
 
