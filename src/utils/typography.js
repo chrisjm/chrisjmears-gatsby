e3bd6dec8fe722d1b39eca68f548cf0d9a105792
Chris Mears <chris.mears@gmail.com>
2019-08-01 15:46:22 -0700

    Fix Google Fonts

--- src/utils/typography.js
+++ src/utils/typography.js
@@ -3,8 +3,24 @@ import Typography from 'typography';
 const typography = new Typography({
   baseFontSize: '16px',
   baseLineHeight: 1.666,
-  headerFontFamily: ['Roboto Slab', 'sans-serif'],
-  bodyFontFamily: ['Montserrat', 'serif'],
+  googleFonts: [
+    {
+      name: 'Montserrat',
+      styles: [
+        '300',
+        '300i',
+        '700',
+      ],
+    },
+    {
+      name: 'Roboto Slab',
+      styles: [
+        '700'
+      ],
+    },
+  ],
+  headerFontFamily: ['Roboto Slab', 'serif'],
+  bodyFontFamily: ['Montserrat', 'sans-serif'],
 });
 
 export default typography;
