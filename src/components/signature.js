fc9ecc92a494f73e4f71927abab289a5365bf6c9
Chris Mears <chris.mears@gmail.com>
2019-08-13 15:31:14 -0700

    Update Signature

--- src/components/signature.js
+++ src/components/signature.js
@@ -1,69 +1,56 @@
 import React from 'react';
+import OutsideLink from './outside-link';
 
 function Signature() {
   return (
     <div className="signature text-base border rounded p-6 mb-10 mx-auto max-w-3xl">
       <span>
-        Hi! I'm Chris J Mears.&nbsp;
+        Hi! I'm Chris J Mears.{' '}
         <span aria-label="wave" role="img">
           👋
         </span>
       </span>
       <span>
-        &nbsp;I'm an independent&nbsp;
-        <a href="https://github.com/chrisjm" target="_blank" rel="noopener noreferrer">
-          full stack web developer
-        </a>
-        ,&nbsp;
-        <a href="https://www.wanderingleafstudios.com/" target="_blank" rel="noopener noreferrer">
-          solopreneur
-        </a>
-        , and&nbsp;
-        <a href="https://chrisjmears.com/" target="_blank" rel="noopener noreferrer">
-          consultant
-        </a>
-        .&nbsp;
+        {' '}
+        I'm an independent{' '}
+        <OutsideLink to="https://github.com/chrisjm" name="full stack web developer" />,{' '}
+        <OutsideLink to="https://www.wanderingleafstudios.com/" name="solopreneur" />, and{' '}
+        <OutsideLink to="https://chrisjmears.com/" name="consultant" />.{' '}
         <span aria-label="geek" role="img">
           🤓
         </span>
       </span>
       <span>
-        &nbsp;My mission is to help non-technical freelancers and solopreneurs gain control of their
-        web technologies and get back to their passion.&nbsp;
+        {' '}
+        My mission is to help non-technical freelancers and solopreneurs gain control of their web
+        technologies and get back to their passion.{' '}
         <span aria-label="arm muscle" role="img">
           💪
         </span>
       </span>
       <span>
-        &nbsp;I built and maintain the&nbsp;
-        <a href="https://www.openbrewerydb.org/" target="_blank" rel="noopener noreferrer">
-          Open Brewery DB
-        </a>
-        &nbsp;open-source project.&nbsp;
+        {' '}
+        I built and maintain the{' '}
+        <OutsideLink to="https://www.openbrewerydb.org/" name="Open Brewery DB" /> open-source
+        project.{' '}
         <span aria-label="beer cheers" role="img">
           🍻
         </span>
       </span>
       <span>
-        &nbsp;I have a rescue dog named Cooper&nbsp;
+        {' '}
+        I have a rescue dog named Cooper{' '}
         <span aria-label="dog" role="img">
           🐶
         </span>
       </span>
       <span>
-        &nbsp;You can follow me on&nbsp;
-        <a href="https://instagram.com/chrisjm" target="_blank" rel="noopener noreferrer">
-          Instagram
-        </a>
-        ,&nbsp;
-        <a href="https://twitter.com/chrisjm" target="_blank" rel="noopener noreferrer">
-          Twitter
-        </a>
-        , and&nbsp;
-        <a href="https://www.linkedin.com/in/chrisjm80/" target="_blank" rel="noopener noreferrer">
-          LinkedIn
-        </a>
-        .&nbsp;
+        {' '}
+        You can follow me on <OutsideLink
+          to="https://instagram.com/chrisjm"
+          name="Instagram"
+        />, <OutsideLink to="https://twitter.com/chrisjm" name="Twitter" />, and{' '}
+        <OutsideLink to="https://www.linkedin.com/in/chrisjm80/" name="LinkedIn" />.{' '}
         <span aria-label="thank you" role="img">
           🙏
         </span>
