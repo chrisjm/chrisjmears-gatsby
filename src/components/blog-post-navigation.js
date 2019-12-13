53346e176ae5f8b3aa6a901e6d044d4fe0d64b21
Chris Mears <chris.mears@gmail.com>
2019-12-12 20:37:03 -0800

    Update blog post navigation

--- src/components/blog-post-navigation.js
+++ src/components/blog-post-navigation.js
@@ -6,45 +6,44 @@ import Section from './section';
 function BlogPostNavigation({ previous, next }) {
   return (
     <Section sectionClasses="bg-blue-600 text-white">
-      <ul className="flex flex-col md:flex-row">
-        <li className="flex-1 md:mr-5">
-          {previous && (
-            <>
-              <Link
-                to={previous.fields.slug}
-                rel="prev"
-                className="hover:text-blue-200 hover:no-underline"
-              >
-                {previous.frontmatter.header && (
-                  <div className="mb-2">
-                    <img src={previous.frontmatter.header.teaser} alt={`Featured`} />
-                  </div>
-                )}
-                <div className="text-lg leading-tight">{previous.frontmatter.title}</div>
-              </Link>
-              <i className="block mt-2">{previous.frontmatter.date}</i>
-            </>
-          )}
-        </li>
-        <li className="flex-1 md:ml-5">
-          {next && (
-            <>
-              <Link
-                to={next.fields.slug}
-                rel="prev"
-                className="hover:text-blue-200 hover:no-underline"
-              >
-                {next.frontmatter.header && (
-                  <div className="mb-2">
-                    <img src={next.frontmatter.header.teaser} alt={`Featured`} />
-                  </div>
-                )}
-                <div className="text-lg leading-tight">{next.frontmatter.title}</div>
-              </Link>
-              <i className="block mt-2">{next.frontmatter.date}</i>
-            </>
-          )}
-        </li>
+      <h2 className="text-3xl mb-6">
+        Additional Posts
+      </h2>
+      <ul className="flex flex-col md:flex-row justify-center">
+        {previous && (
+          <li className="flex-1 max-w-1/2 md:mr-5">
+            <Link
+              to={previous.fields.slug}
+              rel="prev"
+              className="hover:text-blue-200 hover:no-underline"
+            >
+              {previous.frontmatter.header && (
+                <div className="mb-2">
+                  <img src={previous.frontmatter.header.teaser} alt={`Featured`} />
+                </div>
+              )}
+              <div className="text-lg leading-tight">{previous.frontmatter.title}</div>
+            </Link>
+            <i className="block mt-2">{previous.frontmatter.date}</i>
+          </li>
+        )}
+        {next && (
+          <li className="flex-1 max-w-1/2 md:ml-5">
+            <Link
+              to={next.fields.slug}
+              rel="prev"
+              className="hover:text-blue-200 hover:no-underline"
+            >
+              {next.frontmatter.header && (
+                <div className="mb-2">
+                  <img src={next.frontmatter.header.teaser} alt={`Featured`} />
+                </div>
+              )}
+              <div className="text-lg leading-tight">{next.frontmatter.title}</div>
+            </Link>
+            <i className="block mt-2">{next.frontmatter.date}</i>
+          </li>
+        )}
       </ul>
     </Section>
   );
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,15 @@
 // See https://next.tailwindcss.com/docs/configuration for details
 
 module.exports = {
-  theme: {},
+  theme: {
+    extend: {
+      maxWidth: {
+        '1/4': '25%',
+        '1/2': '50%',
+        '3/4': '75%',
+      },
+    },
+  },
   variants: {
     backgroundColor: ['responsive', 'hover', 'focus', 'active'],
   },
