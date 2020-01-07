850c5552e037db976030c1118128ca2611338bc1
Chris Mears <chris.mears@gmail.com>
2020-01-07 15:19:21 -0800

    fix: Darken Heading2 `hr` to match bg

--- src/components/heading2.js
+++ src/components/heading2.js
@@ -4,7 +4,7 @@ function Heading2({ title }) {
   return (
     <div className="mb-10 text-center">
       <h2 className="text-4xl font-bold mb-2">{title}</h2>
-      <hr className="h-1 bg-blue-600" />
+      <hr className="h-1 bg-blue-800" />
     </div>
   );
 }
