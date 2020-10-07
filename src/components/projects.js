c3af929fb45de09bd623bfab2414a562330287da
Chris Mears <chris.mears@gmail.com>
2020-10-06 20:49:19 -0700

    feat: Update projects

--- src/components/projects.js
+++ src/components/projects.js
@@ -5,22 +5,11 @@ import Heading2 from "./heading2"
 
 const projects = [
   {
-    id: "obdb",
-    title: "Open Brewery DB",
-    description:
-      "Open Brewery DB is a free API for public information on breweries, cideries, brewpubs, and bottleshops.",
-    tags: ["data", "api", "ruby-on-rails", "postgresql", "aws", "heroku"],
-    imageUrl:
-      "https://res.cloudinary.com/wanderingleafstudios/image/upload/v1572317242/openbrewerydb/OpenBreweryDBLogo.jpg",
-    imageAlt: "Open Brewery DB Logo",
-    linkUrl: "https://www.openbrewerydb.org",
-  },
-  {
     id: "data-nerd-newsletter",
     title: "Data Nerd Newsletter",
     description:
       "The archives for my weekly newsletter where I breakdown data and machine learning topics.",
-    tags: ["data-science", "machine-learning", "newsletter"],
+    tags: ["newsletter", "data-science", "machine-learning", "artificial-intelligence", "deep-learning"],
     imageUrl:
       "https://res.cloudinary.com/wanderingleafstudios/image/upload/v1587682706/chrisjmears.com/data-nerd-newsletter-og.jpg",
     imageAlt: "Data Nerd Newsletter",
@@ -28,11 +17,23 @@ const projects = [
     linkUrl: "/data-nerd-newsletter",
   },
   {
-    id: "springboard-london-boroughs",
-    title: "Data Analysis - London Boroughs",
+    id: "credit-card-fraud-analysis",
+    title: "Credit Card Fraud Analysis and Detection",
     description:
-      "Exploratory Data Analysis (EDA) to determine the most expensive London borough based on the housing prices of the last two decades",
-    tags: ["python", "pandas", "jupyter notebook", "data cleaning"],
+      "A project that analyzes and fits various classification models to a highly imbalanced credit card fraud dataset.",
+    tags: ["python", "scikit-learn", "machine-learning", "modeling"],
+    imageUrl:
+      "https://res.cloudinary.com/wanderingleafstudios/image/upload/v1602041852/chrisjmears.com/model-evaluation-prauc.png",
+    imageAlt: "Precision Recall AUC model evaluation plot",
+    ctaText: "View Report",
+    linkUrl: "https://github.com/chrisjm/Credit-Card-Fraud-Analysis/blob/master/README.md",
+  },
+  {
+    id: "springboard-london-boroughs",
+    title: "London Boroughs Data Analysis",
+    description:
+      "A simple exploratory data analysis (EDA) to determine the most expensive London borough based on the housing prices of the last two decades.",
+    tags: ["python", "notebook", "data-analysis"],
     imageUrl:
       "https://res.cloudinary.com/wanderingleafstudios/image/upload/v1589346010/chrisjmears.com/most-expensive-london-boroughs.png",
     imageAlt: "Most expensive London boroughs graph",
@@ -40,11 +41,22 @@ const projects = [
     linkUrl: "https://github.com/chrisjm/chrisjmears-gatsby/blob/master/content/notebooks/london-borough-housing-prices-eda.ipynb",
   },
   {
+    id: "obdb",
+    title: "Open Brewery DB",
+    description:
+      "Open Brewery DB is a free dataset and API with public information on breweries, cideries, brewpubs, and bottleshops.",
+    tags: ["open-source", "public-api"],
+    imageUrl:
+      "https://res.cloudinary.com/wanderingleafstudios/image/upload/v1572317242/openbrewerydb/OpenBreweryDBLogo.jpg",
+    imageAlt: "Open Brewery DB Logo",
+    linkUrl: "https://www.openbrewerydb.org",
+  },
+  {
     id: "tea-quiz",
     title: "Tea Quiz",
     description:
       "A small project to demonstrate a simple web-based ReactJS quiz app to collect data.",
-    tags: ["javascript", "reactjs", "netlify", "material-ui"],
+    tags: ["reactjs", "material-ui", "localstorage"],
     imageUrl:
       "https://res.cloudinary.com/wanderingleafstudios/image/upload/v1576648921/chrisjmears.com/christine-wehrmeier-l4MCijLHc0Q-unsplash.jpg",
     imageAlt: "Tea field",
